716d19f8c9fc90cfc629da5264ce9e43b56bf489
alan_wang <sz9751210@gmail.com>
2023-12-29 06:33:31 +0000

    remove old file

--- gcp/region-ip/modules/main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_address" "region_ip" {
-  count        = length(var.addresses)
-  name         = var.addresses[count.index].name
-  address_type = var.addresses[count.index].address_type
-  network_tier = "PREMIUM"
-  region       = var.addresses[count.index].region
-}
